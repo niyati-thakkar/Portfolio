9c1a9a12fe2002e90c97517b499528bba6647607
Your Name <you@example.com>
2023-02-28 23:12:01 +0530

    Update ProjectDoc.js

--- src/Docs/ProjectDoc.js
+++ src/Docs/ProjectDoc.js
@@ -2,81 +2,81 @@ function ProjectDoc(){
     const projects = [
         {
             title : "Newsletter Signup",
-            content : "Newsletter signup project uses Mailchimp services to add users email address to the list.",
+            content : "NewThe newsletter signup project adds user details to the Mailchimp services list.sletter signup project adds user details to the Mailchimp services list.",
             lnk : "https://newsletter-signup-oldp.onrender.com/",
-            im : ""
+             
         },
         {
             title : "Tabs",
-            content : "Tabs let's the user select tabs and display specific content using JavaScript, CSS, and HTML.",
+            content : "Tabs lets the user select from multiple tabs and display specific content using JavaScript, CSS, and HTML.",
             lnk : "https://niyati-thakkar.github.io/Tabs/",
-            im : ""
+             
         },
         {
             title : "Computers",
-            content : "This website displays facts about computers with a box having different densities of shadow class and properties in CSS and bootstrap.",
+            content : "This website displays facts about computers with a box having different shadow classes each present in bootstrap.",
             lnk : "https://niyati-thakkar.github.io/Computers/",
-            im : ""
+             
         },
         {
             title : "Shopping Tabs",
-            content : "It is a simple web app made using Bootstrap and CSS. It uses svg components to display icons and tabs where user can switch between contents. Active components are displayed with active properties.",
+            content : "It is a simple web app made using Bootstrap and CSS. It uses svg components to display icons and tabs where the user can switch between contents. Active components are displayed with active properties.",
             lnk : "https://niyati-thakkar.github.io/ShoppingTabs/",
-            im : ""
+             
         },
         {
             title : "Plugins",
-            content : "This project is built using Bootstrap Plugins. It contains Bootstrap 'card' class. The order of the components remains same irrespective of the user selection.",
+            content : "This project is built using Bootstrap Plugins. It contains the Bootstrap 'card' class. The order of the components remains the same irrespective of the user selection.",
             lnk : "https://niyati-thakkar.github.io/Plugins/",
-            im : ""
+             
         },
         {
             title : "Profile",
-            content : "It is resume built with pure HTML and HTML tables.",
+            content : "It is a resume built with pure HTML and HTML tables.",
             lnk : "https://niyati-thakkar.github.io/Profile/",
-            im : ""
+             
         },
         {
             title : "Dice Game",
-            content : "Dice game rolls two dice by selecting a random number each a declares a winner. It uses CSS, Javascript, and Bootstrap.",
+            content : "The Dice game rolls two dice by selecting a random number each declaring a winner. It uses CSS, Javascript, and Bootstrap.",
             lnk : "https://niyati-thakkar.github.io/Dice-Game/",
-            im : ""
+             
         },
         {
             title : "Daily Journal",
-            content : "",
+            content : "Daily Journal lets the user write and display Journal. It is created using the EJS library and MongoDB database. It consists of a Home page, Contact Page, About Page and Compose page where the user can create a new journal. These composed journals can then be viewed on the Home page as well as individually.",
             lnk : "https://daily-journal-kl8j.onrender.com/",
-            im : ""
+             
         },
         {
             title : "Secrets",
-            content : "",
+            content : "Secrets App allows user Login and Registration using email as well as Google OAuth 2. It stores the data in the MongoDB database.",
             lnk : "https://secrets-2pln.onrender.com/",
-            im : ""
+             
         },
         {
             title : "Keeper App",
-            content : "",
-            lnk : "",
-            im : ""
+            content : "Keeper App is built using React components. It allows the user to create small notes along with the title. These can very easily be created and deleted.",
+            lnk : "https://my-keeper.vercel.app/",
+             
         },
         {
             title : "Tindog",
-            content : "",
+            content : "Tindog is an online web application where we can find friends for our Pets nearby. It is built using various bootstrap components such as carousals, cards, buttons, icons, footers, etc.",
             lnk : "https://niyati-thakkar.github.io/Tindog/",
-            im : ""
+             
         },
         {
             title : "Simon Game",
-            content : "",
+            content : "It's an exciting game of lights and sounds in which players must repeat random sequences of flashing lights by pressing the colored pads in the correct order.",
             lnk : "https://niyati-thakkar.github.io/Simon-Game/",
-            im : ""
+             
         },
         {
             title : "Todo List",
-            content : "",
+            content : "ToDo list is created using express and Nodejs. It lets the user create various lists and stores the list and list items in a database. The items on the list can be deleted by checking the checkbox.",
             lnk : "https://todolist-fw94.onrender.com/",
-            im : ""
+             
         }
 
     ];
