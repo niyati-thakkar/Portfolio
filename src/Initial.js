78267040d64857771c8dc5c54b34a727d967e226
Your Name <95992115+niyati-thakkar@users.noreply.github.com>
2023-03-03 15:30:37 +0530

    working on mobile

--- src/Initial.js
+++ src/Initial.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Initial(){
     return(
         <div className="initial">
-        <h1 className="heading-primary row">HELLO, I AM NIYATI THAKKAR</h1>
+        <h1 className="heading-primary row">HELLO, I AM</h1><h1 className="heading-primary row squeeze">NIYATI THAKKAR</h1>
         <p className="para-primary">I am a Full Stack Web Developer and a Technical Content Writer.</p>
         <div className="buttonInitialParent"><a href="#" className="initialLinks btn btn-lg btn-dark">PROJECTS</a>
         <a href="#" className="initialLinks btn btn-lg btn-dark">ARTICLES</a></div>
