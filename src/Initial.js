681f5f2fbd6637d01a884070ab01f01469363b6f
Your Name <95992115+niyati-thakkar@users.noreply.github.com>
2023-03-11 00:56:53 +0530

    initial changes

--- src/Initial.js
+++ src/Initial.js
@@ -9,7 +9,7 @@ function Initial({homeRef}){
     const navigate = useNavigate();
     return(
         <div className="initial" ref={homeRef}>
-        <div className="heading-primary row">HELLO, I AM</div><div className="heading-primary row squeeze">NIYATI THAKKAR</div>
+        <div className="heading-primary row">HELLO, I AM</div><div className="squeeze heading-primary row">NIYATI THAKKAR</div>
         <div className="para-primary">I code and design websites and curate technical content.</div>
         <div className="buttonInitialParent"><a href="#" onClick={() => navigate("/projects")} className="initialLinks btn btn-lg btn-dark"  target="_blank">PROJECTS</a>
         <a href="#" onClick={() => navigate("/articles")} className="initialLinks btn btn-lg btn-dark"  target="_blank">ARTICLES</a></div>
