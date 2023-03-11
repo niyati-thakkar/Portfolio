717b2e773e166bde2e56634f6f906e2830bc12f2
Your Name <95992115+niyati-thakkar@users.noreply.github.com>
2023-03-11 09:27:18 +0530

    Update AboutMe.js

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,9 +5,9 @@ function AboutMe({aboutRef}){
         <div className="aboutMe row" ref={aboutRef}>
                 <SubHead head="ABOUT"/>
                 <div className="subHeadParentPara col">
-                <div className="subHeadPara">I am currently pursuing my Bachelor's of Engineering in Computer Science from L. D. College of Engineering, Ahmedabad. I am passionate about programming and technology. I enjoy solving DSA problems and solving Sudoku which is a puzzle printed in every single newspaper.</div>
-                <div className="subHeadPara"> Currently, I am exporing <b>Web Development</b> as well as working as a freelance <b>Technical Content Writer</b> at <b>Scaler</b>. I have written articles on complex topics of Data Structures, Algorithms, Programming Languages (Python, C++, Java, CSS, etc.), DBMS, etc.</div>
-                <div className="subHeadPara">I am open to make new connections, opportunities, and projects. Feel free to Connect or Follow on <a href="#">LinkedIn</a>.</div>
+                <div className="subHeadPara">I am currently pursuing my Bachelor's of Engineering in Computer Science from L. D. College of Engineering, Ahmedabad. I am passionate about programming and technology. I enjoy solving DSA problems and Sudoku puzzles.</div>
+                <div className="subHeadPara"> Currently, I am exporing <b>Web Development</b> as well as working as a freelance <b>Technical Content Writer</b> at <b>Scaler</b>. I have written more than 70 articles on complex topics of Data Structures, Algorithms, Programming Languages (Python, C++, Java, CSS, etc.), DBMS, etc.</div>
+                <div className="subHeadPara">I am open to making new connections, opportunities, and projects. Feel free to Connect or Follow on <b><a href="#">LinkedIn</a></b>.</div>
                 </div>
                 <div className="col subHeadParentPara">
                 <div className="skillHead">Techincal Skills</div>
